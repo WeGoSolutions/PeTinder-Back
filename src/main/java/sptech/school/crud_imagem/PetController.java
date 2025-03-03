cd16b6741dfdb8859c0bc2badb3e6a96d1cde8f2
GuiSousa2910 <122701467+GuiSousa2910@users.noreply.github.com>
2025-03-03 09:50:34 -0300

    tentando encontrar imagem no front

--- src/main/java/sptech/school/crud_imagem/PetController.java
+++ src/main/java/sptech/school/crud_imagem/PetController.java
@@ -1,6 +1,7 @@
 package sptech.school.crud_imagem;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,19 +18,19 @@ public class PetController  {
     private PetRepository repository;
 
     @PostMapping
-    public ResponseEntity<?> cadastrarPet(@RequestBody Pet pet) {
+    public ResponseEntity<Pet> cadastrarPet(@RequestBody Pet pet) {
 
         if (pet.getNome() == null || pet.getNome().trim().isEmpty()) {
-            return ResponseEntity.status(400).body("O campo 'nome' é obrigatório.");
+            return ResponseEntity.status(400).build();
         }
         if (pet.getIdade() < 0) {
-            return ResponseEntity.status(400).body("A 'idade' deve ser um valor positivo.");
+            return ResponseEntity.status(400).build();
         }
         if (pet.getPeso() <= 0) {
-            return ResponseEntity.status(400).body("O 'peso' deve ser maior que zero.");
+            return ResponseEntity.status(400).build();
         }
         if (pet.getAltura() <= 0) {
-            return ResponseEntity.status(400).body("A 'altura' deve ser maior que zero.");
+            return ResponseEntity.status(400).build();
         }
 
         Pet petNovo = new Pet();
@@ -43,11 +44,25 @@ public class PetController  {
                 byte[] imagemBytes = Base64.getDecoder().decode(pet.getImagem());
                 petNovo.setImagem(new String(imagemBytes));
             } catch (IllegalArgumentException e) {
-                return ResponseEntity.status(400).body("Formato de imagem inválido.");
+                return ResponseEntity.status(400).build();
             }
         }
 
         Pet petCadastrado = repository.save(petNovo);
         return ResponseEntity.status(201).body(petCadastrado);
+    }
+
+    @GetMapping("/{id}/imagem")
+    public ResponseEntity<byte[]> getImagem(@PathVariable Integer id) {
+        Pet pet = repository.findById(id).orElse(null);
+        if (pet == null || pet.getImagem() == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        byte[] imageBytes = Base64.getDecoder().decode(pet.getImagem());
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(imageBytes);
     }
 }
