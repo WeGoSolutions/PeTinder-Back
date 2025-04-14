8d7a925cf0ef813061b5fe9de35fa27e267446ed
CauanAraruna <cauan.araruna@sptech.school>
2025-04-14 16:43:35 -0300

    alterações finalizadas

--- src/main/java/cruds/Users/controller/dto/request/UserRequestSenhaDTO.java
+++ src/main/java/cruds/Users/controller/dto/request/UserRequestSenhaDTO.java
@@ -1,5 +1,6 @@
 package cruds.Users.controller.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -11,11 +12,23 @@ public class UserRequestSenhaDTO {
     @Pattern(regexp = "^(?=.*[!@#$%^&*(),.?\\\":{}|<>])[A-Za-z0-9!@#$%^&*(),.?\\\":{}|<>]+$")
     private String senha;
 
+    @NotBlank
+    @Email
+    private String email;
+
     public @NotBlank @Size(min = 6) @Pattern(regexp = "^(?=.*[!@#$%^&*(),.?\\\":{}|<>])[A-Za-z0-9!@#$%^&*(),.?\\\":{}|<>]+$") String getSenha() {
         return senha;
     }
 
     public void setSenha(@NotBlank @Size(min = 6) @Pattern(regexp = "^(?=.*[!@#$%^&*(),.?\\\":{}|<>])[A-Za-z0-9!@#$%^&*(),.?\\\":{}|<>]+$") String senha) {
         this.senha = senha;
+    }
+
+    public @NotBlank @Email String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank @Email String email) {
+        this.email = email;
     }
 }
