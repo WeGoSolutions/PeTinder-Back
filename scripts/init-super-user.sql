68d252e935d12dd968ed1176aeca0d3073b3c8f4
FelipeSouza <antonio.souza@sptech.school>
2025-12-07 17:27:58 -0300

    Iniciando o CI/CD no repositório

--- scripts/init-super-user.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO usuario (id, nome, email, senha)
-SELECT UNHEX(REPLACE('11111111-1111-1111-1111-111111111111', '-', '')), 'super', 'super@admin.com', 'senhaSuperHash'
-    WHERE NOT EXISTS (
-    SELECT 1 FROM usuario WHERE id = UNHEX(REPLACE('11111111-1111-1111-1111-111111111111', '-', ''))
-);
